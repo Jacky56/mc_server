2f4e37a215ae982a540bcf27776a5ba5e0614ffa
jacky banh <37816729+Jacky56@users.noreply.github.com>
2022-09-14 13:29:35 +0100

    Update cron_dump_save.sh

--- cron_dump_save.sh
+++ cron_dump_save.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 export zip_name=world_`date +%Y_%m_%d_%H_%M`.zip
-export date_path=world_`date +%Y/%m/%d`.zip
+export date_path=`date +%Y/%m/%d`
 cd ~/minecraft
 zip -r $zip_name world && gsutil cp ~/minecraft/$zip_name gs://potatoe-block-simulator/$date_path/$zip_name
 rm ~/minecraft/$zip_name
