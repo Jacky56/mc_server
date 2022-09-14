8ed5b84a1824f4b50b55c5754030c32c83a1bd2c
jacky banh <37816729+Jacky56@users.noreply.github.com>
2022-09-14 02:06:37 +0100

    Create cron_dump_save.sh

---
+++ cron_dump_save.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+export zip_name=world_`date +%Y_%m_%d_%H_%M`.zip
+cd ~/minecraft
+zip -r $zip_name world && gsutil cp ~/minecraft/$zip_name gs://potatoe-block-simulator/
+rm ~/minecraft/world.zip
